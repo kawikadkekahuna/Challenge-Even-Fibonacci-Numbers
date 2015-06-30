fe62eeb0d2f10530d0c54e372550f427d4c3aefc
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-30 11:52:32 -1000

    added highest fib finder and even fib

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -6,26 +6,78 @@
  * @return {Number} sum
  */
 module.exports = {
-  sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  sumFibs: _sumFibs,
+  highestFibonacciNumber: _highestFibonacciNumber
 };
 
-function _sumFibs( maxFibValue ) {
+function _sumFibs(maxFibValue) {
+  if (typeof maxFibValue !== 'number') {
+    throw new TypeError('Invalid input');
+  }
+  var counter = 0;
   var sum = 0;
+  var last = 0;
+  var next = 0;
+  var current = 1;
+  var fibArr = [];
 
-  // do your work here
+  function generateFibValues() {
+
+    if (next < maxFibValue) {
+      next = last + current;
+      fibArr.push(next);
+      last = current;
+      current = next;
+      generateFibValues();
+    }
+    return fibArr;
+  }
+
+  var fibArr = generateFibValues();
+
+  var fibEvenArr = fibArr.filter(function(current) {
+    return current % 2 === 0;
+  });
+
+  for (var i = 0; i < fibEvenArr.length; i++) {
+    sum += fibEvenArr[i];
+  }
+
 
   return sum;
-}
+};
+
 
 // bonus round
-function highestFibonacciNumber (maxFibValue){
-  var highest = 0;
+function _highestFibonacciNumber(maxFibValue) {
+  var counter = 0;
+  var sum = 0;
+  var last = 0;
+  var next = 0;
+  var current = 1;
+  var fibArr = [];
 
-  //define your base case, validate your input
 
+  function generateFibValues() {
 
-  //do your work here
+    if (next < maxFibValue) {
+      console.log('last', last);
+      console.log('current', current);
+      next = last + current;
+      console.log('next', next);
+      fibArr.push(next);
+      last = current;
+      current = next;
+      generateFibValues();
+    }
+    return fibArr;
+  }
+  var fibArr = generateFibValues();
+  var fibHighest = fibArr.filter(function(current){
+    return current <= maxFibValue;
+  });
 
+  var length = fibHighest.length;
+  highest = fibHighest[length - 1];
   return highest;
 };
